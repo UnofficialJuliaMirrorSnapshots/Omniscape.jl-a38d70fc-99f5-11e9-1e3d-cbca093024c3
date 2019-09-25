3296214209dcb7b41036ac266473eb2bdd962c27
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-25 14:36:27 +0000

    Snapshot of branch dev taken on 2019-09-25T10:36:25.203-04:00 from "https://github.com/UnofficialJuliaMirror/Omniscape.jl-a38d70fc-99f5-11e9-1e3d-cbca093024c3"

--- src/io.jl
+++ src/io.jl
@@ -14,11 +14,13 @@ end
 
 function parse_ascii_header(path::String)
     cf = init_ascii_header()
-    header = open(readlines, `head -n $(6) $(path)`)
+    f = open(path, "r")
     for i = 1:6
-        key_val = split(header[i])
+        key_val = split(readline(f))
+        key_val[1] = lowercase(key_val[1])
         cf["$(key_val[1])"] = key_val[2]
     end
+
     cf
 end
 
